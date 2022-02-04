2cceb1ce26b8c2917780b05c5bebf9c9da1f9505
Carlos  Bettoi <carlosbettoi@yahoo.com>
2022-02-04 18:28:31 -0300

    inclusão  da propriedade getMinutes

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ function carregar() {
     var img = window.document.getElementById ('imagem')
     var data = new Date ()
     var hora = data.getHours ()
-    
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var minutos = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora}:${minutos} horas.`
+        
     if (hora >=0 && hora < 12) {
         img.src = 'fotodia2.png'
         document.body.style.background = '#e2cd9f'
